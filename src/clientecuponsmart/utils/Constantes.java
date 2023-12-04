6358663f54f22f35039af9b4c52b229c8328c26f
Christian <filetelopezchristian@gmail.com>
2023-12-03 22:32:04 -0600

    Cambios no importantes

--- src/clientecuponsmart/utils/Constantes.java
+++ src/clientecuponsmart/utils/Constantes.java
@@ -1,10 +1,12 @@
 package clientecuponsmart.utils;
 
 public class Constantes {
-    public static final int ERROR_URL=101;
-    public static final int ERROR_PETICION=102;
-    public static  String URL_WS="http://localhost:8084/APICuponSmart/api/";
-    public static String CampoInvalido="Rellene el campo";
+
+    public static final int ERROR_URL = 101;
+    public static final int ERROR_PETICION = 102;
+    public static String URL_WS = "http://localhost:8084/APICuponSmart/api/";
+    public static String CampoInvalido = "Rellene el campo";
     public static Integer ID_ROL_COMERCIAL = 1;
     public static Integer ID_ROL_GENERAL = 2;
+    
 }
