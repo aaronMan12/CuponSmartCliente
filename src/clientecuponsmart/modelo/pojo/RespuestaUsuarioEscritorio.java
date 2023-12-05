9af4bf4ddc19c6d609c231924b16543ef09289f8
Christian <filetelopezchristian@gmail.com>
2023-12-04 22:51:43 -0600

    Se agregaron los atributos roles y empresas

--- src/clientecuponsmart/modelo/pojo/RespuestaUsuarioEscritorio.java
+++ src/clientecuponsmart/modelo/pojo/RespuestaUsuarioEscritorio.java
@@ -7,14 +7,34 @@ public class RespuestaUsuarioEscritorio {
     private boolean error;
     private String contenido;
     private List<Usuario> usuarios;
+    private List<Roll> roles;
+    private List<Empresa> empresas;
     
     public RespuestaUsuarioEscritorio() {
     }
 
-    public RespuestaUsuarioEscritorio(boolean error, String contenido, List<Usuario> usuarios) {
+    public RespuestaUsuarioEscritorio(boolean error, String contenido, List<Usuario> usuarios, List<Roll> roles, List<Empresa> empresas) {
         this.error = error;
         this.contenido = contenido;
         this.usuarios = usuarios;
+        this.roles = roles;
+        this.empresas = empresas;
+    }
+
+    public List<Empresa> getEmpresas() {
+        return empresas;
+    }
+
+    public void setEmpresas(List<Empresa> empresas) {
+        this.empresas = empresas;
+    }
+
+    public List<Roll> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Roll> roles) {
+        this.roles = roles;
     }
 
     public boolean isError() {
