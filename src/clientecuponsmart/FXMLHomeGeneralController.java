398149a14c423e2e05b50b085b89a697710b7a91
Christian <filetelopezchristian@gmail.com>
2023-12-07 18:05:23 -0600

    Se cambió el diseño de la vista

--- src/clientecuponsmart/FXMLHomeGeneralController.java
+++ src/clientecuponsmart/FXMLHomeGeneralController.java
@@ -10,9 +10,12 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class FXMLHomeGeneralController implements Initializable {
 
@@ -20,10 +23,22 @@ public class FXMLHomeGeneralController implements Initializable {
 
     @FXML
     private Label lbNombreUsuario;
+    @FXML
+    private Button btnIconUsuarios;
+    @FXML
+    private Button btnIconEmpresas;
+    @FXML
+    private Button btnIconSucursales;
+    @FXML
+    private Button btnIconPromociones;
+    @FXML
+    private Button btnIconCupon;
+    @FXML
+    private Button btnIconCuenta;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        this.cargarImagenes();
     }
 
     public void inicializarHomeGeneral(Usuario usuarioSesion) {
@@ -63,4 +78,44 @@ public class FXMLHomeGeneralController implements Initializable {
     private void btnGestionPromociones(ActionEvent event) {
 
     }
+
+    private void cargarImagenes() {
+        this.cargarImagen(btnIconUsuarios, "/componentes/moduloUsuario.png");
+        this.cargarImagen(btnIconEmpresas, "/componentes/moduloEmpresa.png");
+        this.cargarImagen(btnIconCupon, "/componentes/moduloCupon.png");
+        this.cargarImagen(btnIconSucursales, "/componentes/moduloSucursal.png");
+        this.cargarImagen(btnIconPromociones, "/componentes/moduloPromocion.png");
+        this.cargarImagen(btnIconCuenta, "/componentes/gestionCuenta.png");
+    }
+
+    private void cargarImagen(Button button, String imagePath) {
+        Image image = new Image(getClass().getResource(imagePath).toExternalForm());
+        ImageView imageView = new ImageView(image);
+        button.setGraphic(imageView);
+    }
+
+    @FXML
+    private void btnGestionCupon(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnGestionCuenta(ActionEvent event) {
+        try {
+            FXMLLoader vistaLoad = new FXMLLoader(getClass().getResource("FXMLRegistrarUsuario.fxml"));
+            Parent vista = vistaLoad.load();
+
+            FXMLRegistrarUsuarioController controlador = vistaLoad.getController();
+            controlador.inicializarInformacion(usuarioSesion);
+
+            Stage stage = new Stage();
+            Scene scenaAdmin = new Scene(vista);
+            stage.setScene(scenaAdmin);
+            stage.setTitle("Editar usuario");
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
