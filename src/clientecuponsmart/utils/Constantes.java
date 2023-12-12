d38944cec886d4ac3803903cb6c04a9d2da7208c
Christian <filetelopezchristian@gmail.com>
2023-12-12 15:17:46 -0600

    Se crearon nuevas contantes y se eliminaron algunas

--- src/clientecuponsmart/utils/Constantes.java
+++ src/clientecuponsmart/utils/Constantes.java
@@ -2,11 +2,24 @@ package clientecuponsmart.utils;
 
 public class Constantes {
 
+    // CONSTANTE ENDPOINT API
+    public static String URL_WS = "http://localhost:8084/APICuponSmart/api/";
+
+    // CONSTANTES CODIGO DE ERROR
     public static final int ERROR_URL = 101;
     public static final int ERROR_PETICION = 102;
-    public static String URL_WS = "http://localhost:8084/APICuponSmart/api/";
-    public static String CampoInvalido = "Rellene el campo";
+
+    // CONSTANTES ROL USUARIO
     public static final Integer ID_ROL_COMERCIAL = 1;
     public static final Integer ID_ROL_GENERAL = 2;
+
+    // CONSTANTES ESTATUS
+    public static final String ESTATUS_ACTIVO = "activo";
+    public static final String ESTATUS_INACTIVO = "inactivo";
+
+    // CONSTANTES TIPO PROMOCION
+    public static final String PROMOCION_DESCUENTO = "descuento";
+    public static final String PROMOCION_REBAJADO = "rebajado";
+    
     
 }
