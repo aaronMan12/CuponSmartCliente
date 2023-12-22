14efde948cc5b38fff9aa1ae584ebddee73fd4a9
Christian <filetelopezchristian@gmail.com>
2023-12-21 20:05:48 -0600

    Se implementó un nuevo atributo llamado 'ubicacion' :monkey:

--- src/clientecuponsmart/modelo/pojo/RespuestaUsuarioEscritorio.java
+++ src/clientecuponsmart/modelo/pojo/RespuestaUsuarioEscritorio.java
@@ -2,9 +2,8 @@ package clientecuponsmart.modelo.pojo;
 
 import java.util.List;
 
-
 public class RespuestaUsuarioEscritorio {
-    
+
     private boolean error;
     private String contenido;
     private List<Usuario> usuarios;
@@ -13,11 +12,12 @@ public class RespuestaUsuarioEscritorio {
     private Empresa empresa;
     private List<Sucursal> sucursales;
     private Sucursal sucursal;
-    
+    private Ubicacion ubicacion;
+
     public RespuestaUsuarioEscritorio() {
     }
 
-    public RespuestaUsuarioEscritorio(boolean error, String contenido, List<Usuario> usuarios, List<Roll> roles, List<Empresa> empresas, Empresa empresa, List<Sucursal> sucursales, Sucursal sucursal) {
+    public RespuestaUsuarioEscritorio(boolean error, String contenido, List<Usuario> usuarios, List<Roll> roles, List<Empresa> empresas, Empresa empresa, List<Sucursal> sucursales, Sucursal sucursal, Ubicacion ubicacion) {
         this.error = error;
         this.contenido = contenido;
         this.usuarios = usuarios;
@@ -26,6 +26,15 @@ public class RespuestaUsuarioEscritorio {
         this.empresa = empresa;
         this.sucursales = sucursales;
         this.sucursal = sucursal;
+        this.ubicacion = ubicacion;
+    }
+
+    public Ubicacion getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(Ubicacion ubicacion) {
+        this.ubicacion = ubicacion;
     }
 
     public Sucursal getSucursal() {
@@ -92,5 +101,4 @@ public class RespuestaUsuarioEscritorio {
         this.empresa = empresa;
     }
 
-   
 }
