15df185e40b0bb08dd46043346bda91a90b8df82
aaronMan12 <111460458+aaronMan12@users.noreply.github.com>
2023-12-23 19:22:37 -0600

    modulo promoción1/4

--- src/clientecuponsmart/FXMLAdminPromocionesController.java
+++ src/clientecuponsmart/FXMLAdminPromocionesController.java
@@ -4,33 +4,43 @@ import clientecuponsmart.modelo.dao.PromocionDAO;
 import clientecuponsmart.modelo.pojo.Busqueda;
 import clientecuponsmart.modelo.pojo.Promocion;
 import clientecuponsmart.modelo.pojo.RespuestaUsuarioEscritorio;
+import clientecuponsmart.modelo.pojo.Sucursal;
+import clientecuponsmart.utils.Utilidades;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.function.Predicate;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class FXMLAdminPromocionesController implements Initializable {
-
+    
+    //CONSTANTES PARA FILTRAR
+    private final int BUSQUEDA_POR_FECHA_INICIO = 1;
+    private final int BUSQUEDA_POR_FECHA_FIN = 2;
+    private final int BUSQUEDA_POR_NOMBRE = 3;  
+    
     private ObservableList<Promocion> promociones;
     private FilteredList<Promocion> filteredListPromociones;
 
     private ObservableList<Busqueda> promocionesBusqueda;
-    private Integer idBusquedaselección;
+    private Integer idBusquedaSeleccion;
 
     private Integer idEmpresa;
 
-    @FXML
-    private DatePicker dtBuscarPromocion;
     @FXML
     private ComboBox cbBusqueda;
     @FXML
@@ -57,6 +67,11 @@ public class FXMLAdminPromocionesController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         promociones = FXCollections.observableArrayList();
+        promocionesBusqueda = FXCollections.observableArrayList();
+        configurarColumnasTabla();
+        cargarInformacionBusquedaComboBox();
+        configurarSeleccioBusquedaComboBox();
+        configurarTfBusquedaPromocion();
     }
 
     @FXML
@@ -89,6 +104,7 @@ public class FXMLAdminPromocionesController implements Initializable {
         promociones.addAll(listPromociones);
         filteredListPromociones = new FilteredList<>(promociones);
         tbPromociones.setItems(filteredListPromociones);
+        
 
     }
 
@@ -103,4 +119,72 @@ public class FXMLAdminPromocionesController implements Initializable {
         tbPromociones.setItems(filteredListPromociones);
     }
 
+    private void configurarColumnasTabla(){
+        colNombre.setCellValueFactory(new PropertyValueFactory("nombre"));
+        colCategoria.setCellValueFactory(new PropertyValueFactory("categoria"));
+        colCodigo.setCellValueFactory(new PropertyValueFactory("codigo"));
+        colEstatus.setCellValueFactory(new PropertyValueFactory("estatus"));
+        colFechaFin.setCellValueFactory(new PropertyValueFactory("fechaFin"));
+        colFechaInicio.setCellValueFactory(new PropertyValueFactory("fechaInicio"));
+        colNoCupones.setCellValueFactory(new PropertyValueFactory("noCuponesMaximo"));
+        colTipoPromocion.setCellValueFactory(new PropertyValueFactory("tipoPromocion"));
+    }
+    
+    private void cargarInformacionBusquedaComboBox(){
+        promocionesBusqueda.addAll(new Busqueda(this.BUSQUEDA_POR_NOMBRE, "Busqueda por nombre"),
+                new Busqueda(this.BUSQUEDA_POR_FECHA_INICIO, "Busqueda por fecha inicio"),
+                new Busqueda(this.BUSQUEDA_POR_FECHA_FIN, "Busqueda por fecha fin"));
+        cbBusqueda.setItems(promocionesBusqueda);
+    }
+    
+    private void configurarSeleccioBusquedaComboBox(){
+        cbBusqueda.valueProperty().addListener(new ChangeListener<Busqueda>() {
+            @Override
+            public void changed(ObservableValue<? extends Busqueda> observable, Busqueda oldValue, Busqueda newValue) {
+                if (filteredListPromociones.isEmpty()){
+                    Utilidades.mostrarAlertaSimple("Error", "Lista vacia", Alert.AlertType.ERROR);
+                    return;
+                }
+                idBusquedaSeleccion = newValue.getIdBusqueda();
+                tfBuscarPromocion.setText("");
+              
+            }
+            
+        });
+    }
+    
+    private void configurarTfBusquedaPromocion(){
+        tfBuscarPromocion.textProperty().addListener((textObservable, oldText, newText) ->{
+            if (newText.isEmpty()){
+                filteredListPromociones.setPredicate(null);
+                return;
+            }
+            if (idBusquedaSeleccion == null){
+                tfBuscarPromocion.setText("");
+                Utilidades.mostrarAlertaSimple("Error", "por favor seleccione un tipo de busqueda", Alert.AlertType.ERROR);
+                return;
+            }
+            buscarpromociones(idBusquedaSeleccion, newText);
+        });
+        
+    }
+    
+    private void buscarpromociones(int idBusqueda, String busqueda){
+        Predicate<Promocion> predicado = new Predicate<Promocion>(){
+            @Override
+            public boolean test(Promocion promocion) {
+                switch (idBusqueda) {
+                    case BUSQUEDA_POR_NOMBRE:
+                        return promocion.getNombre().equals(busqueda);
+                    case BUSQUEDA_POR_FECHA_INICIO:
+                        return promocion.getFechaInicio().equals(busqueda);
+                    default:
+                        return promocion.getFechaFin().equals(busqueda);
+                }
+            }
+            
+        };
+        
+        filteredListPromociones.setPredicate(predicado);
+    }
 }
